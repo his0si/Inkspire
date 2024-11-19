2e2f6fe4cb31e7739964bbadd1468b86c92d3554
Hee Seo Kim <hisounni@ewhain.net>
2024-11-19 17:48:08 +0900

    에러 수정

--- client/src/components/views/PostDetail.jsx
+++ client/src/components/views/PostDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const BackButton = styled.div`
   cursor: pointer;
